78b84dfaef6c244d53f2b9ad57c4151f04c0a6e6
nireitdev <nireitdev@c2prana>
2024-07-24 11:08:41 -0300

    new: First tests with implant

--- src/implant/main.go
+++ src/implant/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.dev/nireitdev/go-C2-Prana/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
 
 func main() {
-	fmt.Println("Implant")
+	//Coneccion al server:
+	opts := grpc.DialOption(grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost:3000", opts)
+	if err != nil {
+		log.Fatalln("Imposible conectar con el servidor. Bye!")
+	}
+	defer conn.Close()
+
+	srv := proto.NewServerClient(conn)
+
+	req := proto.ImplantRequest{Request: "Implanted!"}
+	resp, err := srv.ImplantCmd(context.Background(), &req)
+	if err != nil {
+		log.Fatalf("Fallo intercambio de msgs: %v \n", err)
+	}
+
+	log.Println("Recibido:", resp.Result)
 
 }
